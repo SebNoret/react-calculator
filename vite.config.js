b769fef8839d183af0cbbd823d13145fcfd920d1
Sebastien Noret <sebastien.noret@gmail.com>
2023-04-05 00:24:03 +0200

    add minification options in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
-import html from "vite-plugin-html";
-
+import { createHtmlPlugin } from "vite-plugin-html";
 const isProduction = process.env.NODE_ENV === "production";
-const localPortfolioUrl = "http://localhost:3000/calculatrice/demo/";
+const localPortfolioUrl = "http://localhost:1313/calculatrice/demo/";
 const prodPortfolioUrl =
   "https://sebnoret.github.io/portfolio/calculatrice/demo/";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
   /**
    *
    *
    * Change the base url to the local portfolio url if you want to test the app locally
    */
   base: isProduction ? localPortfolioUrl : "/",
-  legacy: {
-    targets: ["defaults", "not IE 11"],
-    additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
-  },
-  html: {
-    minify: true,
+  build: {
+    minify: "esbuild",
   },
 });
